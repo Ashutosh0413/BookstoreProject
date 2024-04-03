91d073af6ff6d252e00937876b5134efe564adac
Ashutosh0413 <patilashutosh67@gmail.com>
2024-04-03 15:13:04 +0530

    Refactored http routes with express router

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,13 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import {Book} from './models/bookModel.js'
+import booksRoute from './routes/booksRoute.js'
+import cors from "cors";
+
 const app = express()
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/',(req,res)=>{
     console.log(req)
@@ -11,25 +16,8 @@ app.get('/',(req,res)=>{
 
 })
 
-app.post('/books',async(request,response)=>{
-    try{
-        if(!request.body.title || !request.body.author || !request.body.publishYear){
-            return response.status(400).send({
-                message: `Send all required fields: title, author, publishYear`,
-            });
-            }
-            const newBook= {
-                title : request.body.title,
-                author: request.body.author, 
-                publishYear: request.body.publishYear,
-            };  
-            const book = await Book.create(newBook);
-            return response.status(201).send(book);            
-    }catch(error){
-        console.log(error.message);
-        response.status(500).send({message:error.message});
-    }
-});
+app.use('/books',booksRoute);
+
 
 mongoose
 .connect(mongoDBURL)
@@ -40,9 +28,6 @@ mongoose
     });
 
 })
-
-
-
 .catch((error)=>{
     console.log(error);
 
